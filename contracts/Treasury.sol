f11817bc29ea6b0ee0b0139a22001e502e930bc2
rahulchauhan3j <r.rahul.c@gmail.com>
2023-01-24 22:16:03 +0530

    updated trasury contract

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -7,30 +7,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // owner of contract should be able to send ether to anyone.
 
 contract Treasury is Ownable {
-    uint256 public totalFunds;
-    address public payee;
-    bool public isReleased;
+  uint256 public totalFunds;
+  address public payee;
+  bool public isReleased;
 
-    event Log(address _addr, uint256 _value);
+  event Log(address _addr, uint256 _value);
 
-    constructor() payable {
-        totalFunds = msg.value;
-        isReleased = false;
-    }
+  constructor() payable {
+    totalFunds = msg.value;
+    isReleased = false;
+  }
 
-    receive() external payable {
-        totalFunds = msg.value;
-        emit Log(msg.sender, msg.value);
-    }
+  receive() external payable {
+    totalFunds += msg.value;
+    emit Log(msg.sender, msg.value);
+  }
 
-    fallback() external payable {
-        totalFunds = msg.value;
-        emit Log(msg.sender, msg.value);
-    }
+  fallback() external payable {
+    totalFunds += msg.value;
+    emit Log(msg.sender, msg.value);
+  }
 
-    function releaseFunds(address _payee) public onlyOwner {
-        isReleased = true;
-        payable(_payee).transfer(totalFunds);
-        totalFunds = 0;
-    }
+  function releaseFunds(
+    address _payee,
+    uint256 fundsToTransfer
+  ) public onlyOwner {
+    require(fundsToTransfer <= totalFunds, "Insufficient Funds in treasury");
+    isReleased = true;
+    payable(_payee).transfer(fundsToTransfer);
+    totalFunds -= fundsToTransfer;
+  }
 }
